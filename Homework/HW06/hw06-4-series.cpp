93d0ea59fa6f9e4b538b7c70b87321981a12a606
Mekha1104 <gtrcooie@gmail.com>
2023-08-12 22:24:24 +0700

    06-4

--- Homework/HW06/hw06-4-series.cpp
+++ Homework/HW06/hw06-4-series.cpp
@@ -16,3 +16,33 @@
         Series = 9 + 99 + 999 + 9999 + 99999 + 999999 + 9999999
         Sum = 11111103
 */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    
+    cout << "Enter number :" << endl;
+    cin >> n;
+
+    int series = 0;
+    int sum = 0;
+    int multiplier = 1;
+
+    for (int i = 1; i <= n; ++i) {
+        series += 9 * multiplier;
+        sum += series;
+        multiplier *= 10;
+
+        if (i < n) {
+            cout << series << " + ";
+        } else {
+            cout << series;
+        }
+    }
+
+    cout << endl;
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
